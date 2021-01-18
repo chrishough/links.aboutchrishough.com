685bb11702c9654d4f8ef966f62f8b356eb101fa
Chris Hough <tech@aboutchrishough.com>
2021-01-18 14:36:46 -0700

    social media section + css

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -1,5 +1,15 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
-library.add();
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons/faTwitterSquare';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare';
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons/faInstagramSquare';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons/faGithubSquare';
+
+library.add(faTwitterSquare,
+            faLinkedin,
+            faFacebookSquare,
+            faInstagramSquare,
+            faGithubSquare);
 
 dom.watch();
