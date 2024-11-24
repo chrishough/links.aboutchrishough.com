e7f49ad37355fed90c7a0a1c2f846fed80d3213c
Chris Hough <tech@aboutchrishough.com>
2024-11-24 16:05:45 -0700

    xxl movie and configuration

--- source/assets/javascripts/pages/index.js
+++ source/assets/javascripts/pages/index.js
@@ -1,19 +1,38 @@
 export default () => {
-  const $videoBackgroundVideo = '/assets/images/backgrounds/biography/20241027-avatar-movie.mp4';
+  $(document).ready(() => {
+    const createVideoHTML = () => `
+      <video autoplay muted playsinline id="crazy-avatar-video-background">
+        <source src="/assets/images/backgrounds/biography/20241027-avatar-movie.mp4" type="video/mp4" id="crazy-avatar-video">
+      </video>
+      <div id="crazy-avatar-fade-background-overlay"></div>
+    `;
 
-  $('#crazy-avatar-video').attr('src', $videoBackgroundVideo);
-  $('#crazy-avatar-video-background').get(0).load();
+    const $videoWrapper = $('#crazy-avatar-video-wrapper');
 
-  Breakpoints.on('xxl xxxl', {
-    enter() {
-      if ($('#crazy-avatar-video').attr('src').trim().length === 0) {
-        $('#crazy-avatar-video').attr('src', $videoBackgroundVideo);
+    const addVideo = () => {
+      if ($videoWrapper.find('#crazy-avatar-video-background').length === 0) {
+        $videoWrapper.html(createVideoHTML());
         $('#crazy-avatar-video-background').get(0).load();
       }
-    },
+    };
 
-    leave() {
-      $('#crazy-avatar-video').attr('src', '');
-    },
+    const removeVideo = () => {
+      $videoWrapper.empty();
+    };
+
+    if (Breakpoints.is('xxl') || Breakpoints.is('xxxl')) {
+      addVideo();
+    } else {
+      removeVideo();
+    }
+
+    Breakpoints.on('xxl xxxl', {
+      enter() {
+        addVideo();
+      },
+      leave() {
+        removeVideo();
+      },
+    });
   });
 };
