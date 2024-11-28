d5b510bbfe9739b8762fb4680de0e8ed0be2d552
chrishough <chrishough@users.noreply.github.com>
2024-11-28 04:05:56 +0000

    deploy: 0d4cd65495a660830cd5f644ffaa44ff7e9d43a4

---
+++ assets/site-96d06426e72f8002650c.js
@@ -0,0 +1,83 @@
+(self.webpackChunk = self.webpackChunk || []).push([ [ 378 ], {
+  /***/ 8204: 
+  /***/ function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+    "use strict";
+    // ./source/assets/javascripts/vendor/breakpoints.js
+    /* harmony default export */ var $ = __webpack_require__(2726), pages_$ = __webpack_require__(2726), site_$ = __webpack_require__(2726);
+    // My Code Only!
+    site_$(document).ready((function() {
+      var $bodyId = site_$("body").attr("id");
+      if (
+      // This MUST! match SCSS: $grid-breakpoints
+      Breakpoints({
+        xs: {
+          min: 0,
+          max: 576
+        },
+        sm: {
+          min: 576,
+          max: 768
+        },
+        md: {
+          min: 768,
+          max: 992
+        },
+        lg: {
+          min: 992,
+          max: 1200
+        },
+        xl: {
+          min: 1200,
+          max: 2e3
+        },
+        xxl: {
+          min: 2e3,
+          max: 2500
+        },
+        xxxl: {
+          min: 2500,
+          max: 1 / 0
+        }
+      }), $(document).ready((function() {
+        var $loader = $("#page-loader"), animationCompleted = !1;
+        $(".loader").length, $(".loader").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", (function() {
+          animationCompleted = !0, $loader.addClass("hidden"), setTimeout((function() {
+            $loader.hide();
+          }), 500);
+        })), setTimeout((function() {
+          !animationCompleted && $loader.is(":visible") && ($loader.addClass("hidden"), setTimeout((function() {
+            $loader.hide();
+          }), 500));
+        }), 2e3);
+      })), "index" === $bodyId) pages_$(document).ready((function() {
+        var $videoWrapper = pages_$("#crazy-avatar-video-wrapper"), addVideo = function addVideo() {
+          0 === $videoWrapper.find("#crazy-avatar-video-background").length && ($videoWrapper.html('\n      <video autoplay muted playsinline id="crazy-avatar-video-background">\n        <source src="/assets/images/backgrounds/biography/20241027-avatar-movie.mp4" type="video/mp4" id="crazy-avatar-video">\n      </video>\n      <div id="crazy-avatar-fade-background-overlay"></div>\n    '), 
+          pages_$("#crazy-avatar-video-background").get(0).load());
+        }, removeVideo = function removeVideo() {
+          $videoWrapper.empty();
+        };
+        Breakpoints.is("xxl") || Breakpoints.is("xxxl") ? addVideo() : removeVideo(), Breakpoints.on("xxl xxxl", {
+          enter: function enter() {
+            addVideo();
+          },
+          leave: function leave() {
+            removeVideo();
+          }
+        });
+      }));
+    }));
+  },
+  /***/ 1597: 
+  /***/ function(module) {
+    module.exports = "/assets/site-05ea3751194ba7db81f53728f8407ed5.css";
+    /***/  }
+}, 
+/******/ function(__webpack_require__) {
+  // webpackRuntimeModules
+  /******/ var __webpack_exec__ = function(moduleId) {
+    return __webpack_require__(__webpack_require__.s = moduleId);
+  }
+  /******/;
+  __webpack_exec__(1597), __webpack_exec__(8204);
+} ]);
+//# sourceMappingURL=site-96d06426e72f8002650c.js.map
